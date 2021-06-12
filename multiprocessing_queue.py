28454d2246e746c3c1a30020e71711dfb36e2f07
Ruzil Zagirov <85795448+ruzzzzzza@users.noreply.github.com>
2021-06-12 17:49:43 +0300

    Edit
    Fixed the code, sometimes the cycle ended early (when procces_two started earlier than process_one)

--- multiprocessing_queue.py
+++ multiprocessing_queue.py
@@ -9,11 +9,14 @@ def creator(q, n):
         q.put(item)
 
 
-def consumer(q):
+def consumer(q, n):
     plus = 0
-    while q.empty() is False:
-        a = q.get()
-        plus += a
+    while True:
+        if q.empty() is False:
+            a = q.get()
+            plus += a
+            if a == n - 1:
+                break
     print(plus)
 
 
@@ -21,7 +24,7 @@ if __name__ == '__main__':
     q = Queue()
     n = 10
     process_one = Process(target=creator, args=(q, n))
-    process_two = Process(target=consumer, args=(q,))
+    process_two = Process(target=consumer, args=(q, n))
 
     process_one.start()
     process_two.start()
@@ -31,3 +34,5 @@ if __name__ == '__main__':
 
     process_one.join()
     process_two.join()
+
+    print(time.time() - start_time)
