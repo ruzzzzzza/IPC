896c1dbd514464e19738ccf4417c74e6d1080891
Ruzil Zagirov <85795448+ruzzzzzza@users.noreply.github.com>
2021-06-14 21:12:10 +0300

    1 to n
    Some problems with global values

--- multiprocessing_queue.py
+++ multiprocessing_queue.py
@@ -1,44 +1,45 @@
-from multiprocessing import Process, Queue
 import time
+from multiprocessing import Process, Queue
 
 start_time = time.time()
+kolvo = 0
+n = 1000
+sm = 0
 
 
-def creator(q, n, start, step):
-    for item in range(start, n, step):
+def creator(q):
+    global n
+    for item in range(n):
         q.put(item)
 
 
-def consumer(q, n):
-    plus = 0
-    k = 0
+def consumer(q):
+    global kolvo, n, sm
     while True:
         if q.empty() is False:
             a = q.get()
-            plus += a
-            k += 1
-            if k == n:
+            kolvo += 1
+            sm += a
+            if kolvo == n:
                 break
-    print(plus)
 
 
 if __name__ == '__main__':
     q = Queue()
-    n = 10000
-    k = 10  # number of processes
+    k = 10
     l = []
+    last = Process(target=creator, args=(q,))
     for i in range(k):
-        l.append(Process(target=creator, args=(q, n, i, k)))
-    last = Process(target=consumer, args=(q, n))
+        l.append(Process(target=consumer, args=(q,)))
+    last.start()
     for p in l:
         p.start()
-    last.start()
 
     q.close()
     q.join_thread()
 
+    last.join()
     for p in l:
         p.join()
-    last.join()
-
+    print(sm)
     print(time.time() - start_time)
