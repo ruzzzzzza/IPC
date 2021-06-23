934b1ff2b0965b2499be145812c656c4995e3504
Ruzil Zagirov <85795448+ruzzzzzza@users.noreply.github.com>
2021-06-23 19:57:09 +0300

    1 to 10, 200B, throughput

--- testing_queue.py
+++ testing_queue.py
@@ -4,45 +4,43 @@ import time
 start_time = time.time()
 
 
-def creator(q, n, l):
+def creator(q, n, l, k):
     str = ''
     for i in range(l):
         str = str + 'a'
     for i in range(n):
         q.put(str)
-    q.put(-1)
+    for i in range(k):
+        q.put(-1)
 
 
-def consumer(q, k):
-    s = 0
+def consumer(q):
     while True:
         if q.empty() is False:
             a = q.get()
             if a == -1:
-                s += 1
-            if s == k:
                 break
 
 
 if __name__ == '__main__':
     q = Queue()
-    l = 100 * 1024  # the size of the string in bytes
+    l = 200  # the size of the string in bytes
     n = 1000000  # number of lines
     k = 10  # number of processes
     kk = []
+    main_process = Process(target=creator, args=(q, n, l, k))
     for i in range(k):
-        kk.append(Process(target=creator, args=(q, int(n / k), l)))
-    last_process = Process(target=consumer, args=(q, k))
+        kk.append(Process(target=consumer, args=(q,)))
 
+    main_process.start()
     for p in kk:
         p.start()
-    last_process.start()
 
     q.close()
     q.join_thread()
 
+    main_process.join()
     for p in kk:
         p.join()
-    last_process.join()
 
     print(time.time() - start_time)
