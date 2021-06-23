e9228a9022c6c0bdfb45af1c557c6bedaaef0cac
Ruzil Zagirov <85795448+ruzzzzzza@users.noreply.github.com>
2021-06-23 18:56:42 +0300

    1 to 1, 200B, throughput

---
+++ testing_queue.py
@@ -0,0 +1,40 @@
+from multiprocessing import Process, Queue
+import time
+
+start_time = time.time()
+
+
+def creator(q, n, l):
+    str = ''
+    for i in range(l):
+        str = str + 'a'
+    for i in range(n):
+        q.put(str)
+    q.put(-1)
+
+
+def consumer(q):
+    while True:
+        if q.empty() is False:
+            a = q.get()
+            if a == -1:
+                break
+
+
+if __name__ == '__main__':
+    q = Queue()
+    l = 200  # the size of the string in bytes
+    n = 1000000  # number of lines
+    process_one = Process(target=creator, args=(q, n, l))
+    process_two = Process(target=consumer, args=(q,))
+
+    process_one.start()
+    process_two.start()
+
+    q.close()
+    q.join_thread()
+
+    process_one.join()
+    process_two.join()
+
+    print(time.time() - start_time)
