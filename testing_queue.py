d281c7e5135036df1b79ac903c4080c7304e337b
Ruzil Zagirov <85795448+ruzzzzzza@users.noreply.github.com>
2021-06-24 20:11:48 +0300

    1 to 1, 200B, delay

--- testing_queue.py
+++ testing_queue.py
@@ -4,43 +4,45 @@ import time
 start_time = time.time()
 
 
-def creator(q, n, l, k):
+class pack:
+
+    def __init__(self, payload, timestamp):
+        self.payload = payload
+        self.timestamp = timestamp
+
+
+def creator(q, n, l):
     str = ''
     for i in range(l):
         str = str + 'a'
     for i in range(n):
-        q.put(str)
-    for i in range(k):
-        q.put(-1)
+        q.put(pack(str, time.time()))
+    q.put(-1)
 
 
-def consumer(q):
+def consumer(q, n):
+    sum = 0
     while True:
         if q.empty() is False:
             a = q.get()
             if a == -1:
                 break
+            sum += time.time() - a.timestamp
+    print(sum / n)
 
 
 if __name__ == '__main__':
     q = Queue()
-    l = 100 * 1024  # the size of the string in bytes
+    l = 200  # the size of the string in bytes
     n = 1000000  # number of lines
-    k = 10  # number of processes
-    kk = []
-    main_process = Process(target=creator, args=(q, n, l, k))
-    for i in range(k):
-        kk.append(Process(target=consumer, args=(q,)))
+    process_one = Process(target=creator, args=(q, n, l))
+    process_two = Process(target=consumer, args=(q, n))
 
-    main_process.start()
-    for p in kk:
-        p.start()
+    process_one.start()
+    process_two.start()
 
     q.close()
     q.join_thread()
 
-    main_process.join()
-    for p in kk:
-        p.join()
-
-    print(time.time() - start_time)
+    process_one.join()
+    process_two.join()
