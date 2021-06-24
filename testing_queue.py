9c2e1124dbca79cceca5b55bb86fb3b44ee170af
Ruzil Zagirov <85795448+ruzzzzzza@users.noreply.github.com>
2021-06-24 21:23:53 +0300

    1 to 10, 200B, delay

--- testing_queue.py
+++ testing_queue.py
@@ -11,47 +11,53 @@ class pack:
         self.timestamp = timestamp
 
 
-def creator(q, n, l):
+def creator(q, n, l, k):
     str = ''
     for i in range(l):
         str = str + 'a'
     for i in range(n):
         q.put(pack(str, time.time()))
-    q.put(-1)
+    for i in range(k):
+        q.put(-1)
 
 
-def consumer(q, k, n):
-    s = 0
+def consumer(q, q1):
     sum = 0
     while True:
         if q.empty() is False:
             a = q.get()
             if a == -1:
-                s += 1
-            else:
-                sum += time.time() - a.timestamp
-            if s == k:
                 break
-    print(sum / n)
+            sum += time.time() - a.timestamp
+    q1.put(sum)
 
 
 if __name__ == '__main__':
     q = Queue()
-    l = 100 * 1024  # the size of the string in bytes
+    q1 = Queue()
+    l = 200  # the size of the string in bytes
     n = 1000000  # number of lines
     k = 10  # number of processes
     kk = []
+    main_process = Process(target=creator, args=(q, n, l, k))
     for i in range(k):
-        kk.append(Process(target=creator, args=(q, int(n / k), l)))
-    last_process = Process(target=consumer, args=(q, k, n))
+        kk.append(Process(target=consumer, args=(q, q1)))
 
+    main_process.start()
     for p in kk:
         p.start()
-    last_process.start()
+
+    sum = 0
+    for i in range(k):
+        sum += q1.get()
 
     q.close()
     q.join_thread()
+    q1.close()
+    q1.join_thread()
 
+    main_process.join()
     for p in kk:
         p.join()
-    last_process.join()
+
+    print(sum / n)
