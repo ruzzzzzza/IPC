7d463d246206bfe60529dd467779358c20542925
Ruzil Zagirov <85795448+ruzzzzzza@users.noreply.github.com>
2021-06-24 21:12:54 +0300

    10 to 1, 200B, delay

--- testing_queue.py
+++ testing_queue.py
@@ -20,29 +20,38 @@ def creator(q, n, l):
     q.put(-1)
 
 
-def consumer(q, n):
+def consumer(q, k, n):
+    s = 0
     sum = 0
     while True:
         if q.empty() is False:
             a = q.get()
             if a == -1:
+                s += 1
+            else:
+                sum += time.time() - a.timestamp
+            if s == k:
                 break
-            sum += time.time() - a.timestamp
     print(sum / n)
 
 
 if __name__ == '__main__':
     q = Queue()
-    l = 100 * 1024  # the size of the string in bytes
+    l = 200  # the size of the string in bytes
     n = 1000000  # number of lines
-    process_one = Process(target=creator, args=(q, n, l))
-    process_two = Process(target=consumer, args=(q, n))
+    k = 10  # number of processes
+    kk = []
+    for i in range(k):
+        kk.append(Process(target=creator, args=(q, int(n / k), l)))
+    last_process = Process(target=consumer, args=(q, k, n))
 
-    process_one.start()
-    process_two.start()
+    for p in kk:
+        p.start()
+    last_process.start()
 
     q.close()
     q.join_thread()
 
-    process_one.join()
-    process_two.join()
+    for p in kk:
+        p.join()
+    last_process.join()
